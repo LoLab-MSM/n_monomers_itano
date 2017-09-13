7ff9957eac13134c0c037ffc3e929616a35c81ea
oscar <oo.ortega2247@gmail.com>
2017-09-12 20:01:13 -0500

    improved visualization

--- example_3monomers.py
+++ example_3monomers.py
@@ -18,15 +18,16 @@ t = sympy.Symbol('t')
 plt.figure(1)
 exp3 = mon3.get_lu_m_sol(3)[0]
 f3 = sympy.lambdify(t, exp3, 'numpy')
-plt.plot(tspan, f3(tspan), color='r', linestyle=':', linewidth=4)
+plt.plot(tspan, f3(tspan), color='r', linestyle=':', linewidth=6, label='LU_3')
 plt.plot(tspan, y['__s2'], color='b')
 
 exp2 = mon3.get_lu_m_sol(2)[0]
 f2 = sympy.lambdify(t, exp2, 'numpy')
-plt.plot(tspan, f2(tspan), color='r', linestyle=':', linewidth=4)
+plt.plot(tspan, f2(tspan), color='k', linestyle=':', linewidth=6, label='LU_2')
 plt.plot(tspan, y['__s1'] + y['__s5'], color='b')
 
 exp1 = mon3.get_lu_m_sol(1)[0]
 f1 = sympy.lambdify(t, exp1, 'numpy')
-plt.plot(tspan, f1(tspan), color='r', linestyle=':', linewidth=4)
-plt.plot(tspan, y['__s0'] + y['__s4'] + y['__s8'], color='b')
+plt.plot(tspan, f1(tspan), color='g', linestyle=':', linewidth=6, label='LU_1')
+plt.plot(tspan, y['__s0'] + y['__s4'] + y['__s8'], color='b', label='simulation')
+plt.legend()
